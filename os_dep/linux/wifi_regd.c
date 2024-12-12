d5debe06fbfa586ff750a8994ca420f5d46db414
Shen Mintao <cx330.shen@autocore.ai>
2024-12-12 09:35:03 +0800

    fix: fix codes for old kernel

--- os_dep/linux/wifi_regd.c
+++ os_dep/linux/wifi_regd.c
@@ -975,7 +975,11 @@ static void async_cac_change_work_hdl(_workitem *work)
 		evt = LIST_CONTAINOR(list, struct async_cac_change_evt, list);
 
 		rtnl_lock();
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 12, 0))
 		cfg80211_cac_event(evt->netdev, &evt->chandef, evt->event, GFP_KERNEL, 0);
+#else
+		cfg80211_cac_event(evt->netdev, &evt->chandef, evt->event, GFP_KERNEL);
+#endif
 		rtnl_unlock();
 
 		rtw_mfree(evt, sizeof(*evt));
@@ -1065,7 +1069,11 @@ static void rtw_cfg80211_cac_event(struct rf_ctl_t *rfctl, u8 band_idx
 		if (async)
 			cfg80211_cac_event_async(iface->pnetdev, &chdef, event);
 		else
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 12, 0))
 			cfg80211_cac_event(iface->pnetdev, &chdef, event, GFP_KERNEL, 0);
+#else
+			cfg80211_cac_event(iface->pnetdev, &chdef, event, GFP_KERNEL);
+#endif
 	}
 }
 
@@ -1103,7 +1111,11 @@ void rtw_cfg80211_cac_finished_event(struct rf_ctl_t *rfctl, u8 band_idx
 		if (!iface || !(ifbmp & BIT(iface->iface_id)))
 			continue;
 		/* finish only for wdev with cac_started */
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 12, 0))
 		if (!iface->rtw_wdev || !iface->rtw_wdev->links[0].cac_started)
+#else
+		if (!iface->rtw_wdev || !iface->rtw_wdev->cac_started)
+#endif
 			ifbmp &= ~BIT(iface->iface_id);
 	}
 
@@ -1126,7 +1138,11 @@ void rtw_cfg80211_cac_aborted_event(struct rf_ctl_t *rfctl, u8 band_idx
 		if (!iface || !(ifbmp & BIT(iface->iface_id)))
 			continue;
 		/* abort only for wdev with cac_started */
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 12, 0))
 		if (!iface->rtw_wdev || !iface->rtw_wdev->links[0].cac_started)
+#else
+		if (!iface->rtw_wdev || !iface->rtw_wdev->cac_started)
+#endif
 			ifbmp &= ~BIT(iface->iface_id);
 	}
 
@@ -1213,9 +1229,17 @@ void rtw_cfg80211_cac_force_finished(struct rf_ctl_t *rfctl, u8 band_idx
 			started_ifbmp &= ~BIT(iface->iface_id);
 			continue;
 		}
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 12, 0))
 		if (need_start && iface->rtw_wdev->links[0].cac_started)
+#else
+		if (need_start && iface->rtw_wdev->cac_started)
+#endif
 			started_ifbmp &= ~BIT(iface->iface_id);
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 12, 0))
 		else if (!need_start && !iface->rtw_wdev->links[0].cac_started)
+#else
+		else if (!need_start && !iface->rtw_wdev->cac_started)
+#endif
 			finished_ifbmp &= ~BIT(iface->iface_id);
 	}
 
